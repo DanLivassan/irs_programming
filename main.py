dac0f0879ebf8b5e0b35d62cd725a8574a0c0c99
Danilo Santana <daniloxc@msn.com>
2021-02-27 16:59:13 -0300

    Show error message when the user call the action incorrectly

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import sys
 
 
 if __name__ == "__main__":
-    parser = IrsParser(sys.argv[2])
-    parsed_args = vars(parser.get_parser().parse_args())
-    irs_validator = IrsParseValidator(parsed_args['action'])
+    try:
+        parser = IrsParser(sys.argv[2]).get_parser()
+        if parser:
+            parsed_args = vars(parser.parse_args())
+            irs_validator = IrsParseValidator(parsed_args['action'])
 
-    if irs_validator.validate(parsed_args):
-        if parsed_args['action'] == constants.DOWNLOAD:
-            min_year, max_year = parsed_args['year_range'].split('-')
-            irs_application.action_download(parsed_args['form_number'], int(min_year), int(max_year))
-        elif parsed_args['action'] == constants.GET_JSON:
-            irs_application.action_get_json(parsed_args['form_numbers'])
+            if irs_validator.validate(parsed_args):
+                if parsed_args['action'] == constants.DOWNLOAD:
+                    min_year, max_year = parsed_args['year_range'].split('-')
+                    irs_application.action_download(parsed_args['form_number'], int(min_year), int(max_year))
+                elif parsed_args['action'] == constants.GET_JSON:
+                    irs_application.action_get_json(parsed_args['form_numbers'])
+        else:
+            print("This action does not exist. Read the docs at: https://github.com/DanLivassan/irs_programming")
+    except IndexError:
+        print("The arguments are incorrect. Read the docs at: https://github.com/DanLivassan/irs_programming")
